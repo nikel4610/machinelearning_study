46da32de8a135081c6cb8b324118953154866506
Hyun - Woo <nikel4610@gmail.com>
2022-01-04 17:30:35 +0900

    Update Linear_Square.py

--- basic_of_deeplearning/Linear_Square.py
+++ basic_of_deeplearning/Linear_Square.py
@@ -6,8 +6,8 @@ x = [2, 4, 6, 8]
 y = [81, 93, 91, 97]
 
 # x 와 y의 평균값
-mx = np.mean(x)
-my = np.mean(y)
+mx = np.mean(x) # mx 는 x의 평균
+my = np.mean(y) # my 는 y의 평균
 print("x와 y의 평균값: ",mx , my)
 
 # 기울기 공식의 분모
@@ -17,13 +17,13 @@ divisor = sum([(mx - i) ** 2 for i in x])
 def top(x, mx, y, my):
     d = 0
     for i in range(len(x)):
-        d += (x[i] - mx) * (y[i] - my)
+        d += (x[i] - mx) * (y[i] - my) # x 와 y 의 편차를 곱해서 합
     return d
 dividend = top(x, mx, y, my)
 
 print("분모, 분자: ", divisor, dividend)
 
-# 기울기와 y절편 구하기
+# 기울기 (a) 와 y절편 (b) 구하기
 a = dividend / divisor
 b = my - (mx * a)
 print(" 기울기 a, y 절편 b: ", a, b)
