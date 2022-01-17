453d4193609bc8ab561d3aa145430ba98dc83f11
Hyun - Woo <nikel4610@gmail.com>
2022-01-17 17:07:52 +0900

    Update 01_My_first_deeplearning.py

--- 01_basic_of_deeplearning/01_My_first_deeplearning.py
+++ 01_basic_of_deeplearning/01_My_first_deeplearning.py
@@ -8,7 +8,7 @@ np.random.seed(3)
 tf.random.set_seed(3)
 
 # 준비된 데이터셋 부르기
-Data_set = np.loadtxt("D:/project/untitled1/dataset/ThoraricSurgery.csv", delimiter=",")
+Data_set = np.loadtxt("ThoraricSurgery.csv", delimiter=",")
 
 # 환자의 기록과 수술 결과 구분하여 설정
 X = Data_set[:, 0:17]
@@ -17,7 +17,7 @@ Y = Data_set[:, 17]
 # 딥러닝 모델 설정
 model = Sequential()
 # 출력층 /. 노드30개, 데이터에서 받을 17개의 값으로 30개 노드로 보냄
-model.add(Dense(30, imput_dim = 17, activation='relu'))
+model.add(Dense(30, input_dim=17, activation='relu'))
 # 출력층 /. 노드 1개
 model.add(Dense(1, activation='sigmoid'))
 
@@ -25,3 +25,6 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 # 10번씩 끊어서 총 100번 실행 반복
 model.fit(X, Y, epochs=100, batch_size=10)
+
+# Epoch 100/100
+# 47/47 [==============================] - 0s 4ms/step - loss: 0.1173 - accuracy: 0.8596
