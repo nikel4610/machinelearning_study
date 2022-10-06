7c805e33e32cf0f134911a1456e138e522524af9
Hyun-woo <nikel4610@gmail.com>
2022-10-06 16:01:33 +0900

    gbm hyper parameter

--- machine_learning/1006/10061.py
+++ machine_learning/1006/10061.py
@@ -1,6 +1,5 @@
 # Ensemble Learning Rules!
 
-# Voting Classifier
 import pandas as pd
 
 from sklearn.ensemble import VotingClassifier
@@ -9,6 +8,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import warnings
+
+from sklearn.model_selection import GridSearchCV
+
+# Voting Classifier
 
 # cancer = load_breast_cancer()
 # data_df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
@@ -37,7 +44,9 @@ from sklearn.metrics import accuracy_score
 # # LogisticRegression 정확도: 0.9386
 # # KNeighborsClassifier 정확도: 0.9386
 
-# Bagging Classifier
+# --------------------------------------------------
+
+# # Bagging Classifier
 def get_new_feature_name_df(old_feature_name_df):
     feature_dup_df = pd.DataFrame(data=old_feature_name_df.groupby('column_name').cumcount(), columns=['dup_cnt'])
     feature_dup_df = feature_dup_df.reset_index()
@@ -71,48 +80,85 @@ def get_human_dataset( ):
     return X_train, X_test, y_train, y_test
 
 
-X_train, X_test, y_train, y_test = get_human_dataset()
+# X_train, X_test, y_train, y_test = get_human_dataset()
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+# warnings.filterwarnings('ignore')
+
+# # 결정 트리에서 사용할 get_human_dataset()을 이용해 학습/테스트용 DataFrame 반환
+# X_train, X_test, y_train, y_test = get_human_dataset()
+
+# # 랜덤 포레스트 학습 및 별도의 테스트셋으로 예측 성능 평가
+# rf_clf = RandomForestClassifier(random_state=0)
+# rf_clf.fit(X_train, y_train)
+# pred = rf_clf.predict(X_test)
+# accuracy = accuracy_score(y_test, pred)
+# print('랜덤 포레스트 정확도: {0:.4f}'.format(accuracy))
+# # 랜덤 포레스트 정확도: 0.9253
+
+# # params = {
+# #     'n_estimators': [20],
+# #     'max_depth': [6, 8, 10, 12],
+# #     'min_samples_leaf': [8, 12, 18],
+# #     'min_samples_split': [8, 16, 20]
+# # }
+
+# # # RandomForestClassifier 객체 생성 후 GridSearchCV 수행 (n_jobs = -1 -> 모든 CPU 코어를 이용해 학습)
+# # rf_clf = RandomForestClassifier(random_state=0, n_jobs=-1)
+# # grid_cv = GridSearchCV(rf_clf, param_grid=params, cv=2, n_jobs=-1)
+# # grid_cv.fit(X_train, y_train)
+
+# # print('최적 하이퍼 파라미터: ', grid_cv.best_params_)
+# # print('최고 예측 정확도: {0:.4f}'.format(grid_cv.best_score_))
+# # 최적 하이퍼 파라미터:  {'max_depth': 12, 'min_samples_leaf': 18, 'min_samples_split': 8, 'n_estimators': 10}
+# # 최고 예측 정확도: 0.9032
+
+# # 최적 하이퍼 파라미터로 재학습 및 예측/평가
+# rf_clf = RandomForestClassifier(n_estimators=10, max_depth=12, min_samples_leaf=18, min_samples_split=8, random_state=0)
+
+# rf_clf.fit(X_train, y_train)
+# pred = rf_clf.predict(X_test)
+# print('예측 정확도: {0:.4f}'.format(accuracy_score(y_test, pred)))
+# # 예측 정확도: 0.9006
+
+# --------------------------------------------------
+
+# Boosting -> 실무에서는 이거 씀 (시간 오래걸림)
+# GBM  Hyper Parameter
+from sklearn.ensemble import GradientBoostingClassifier
+import time
 import warnings
 warnings.filterwarnings('ignore')
 
-# 결정 트리에서 사용할 get_human_dataset()을 이용해 학습/테스트용 DataFrame 반환
-X_train, X_test, y_train, y_test = get_human_dataset()
+x_train, x_test, y_train, y_test = get_human_dataset()
 
-# 랜덤 포레스트 학습 및 별도의 테스트셋으로 예측 성능 평가
-rf_clf = RandomForestClassifier(random_state=0)
-rf_clf.fit(X_train, y_train)
-pred = rf_clf.predict(X_test)
-accuracy = accuracy_score(y_test, pred)
-print('랜덤 포레스트 정확도: {0:.4f}'.format(accuracy))
-# 랜덤 포레스트 정확도: 0.9253
+# GBM 수행시간 측정
+start_time = time.time()
+
+gb_clf = GradientBoostingClassifier(random_state=0)
+gb_clf.fit(x_train, y_train)
+# gb_pred = gb_clf.predict(x_test)
+# gb_accuracy = accuracy_score(y_test, gb_pred)
+
+# print('GBM 정확도: {0:.4f}'.format(gb_accuracy))
+# print('GBM 수행시간: {0:.1f} 초'.format(time.time() - start_time))
+# GBM 정확도: 0.9389
+# GBM 수행시간: 442.2 초
+
+# --------------------------------------------------
 
 from sklearn.model_selection import GridSearchCV
 
-# params = {
-#     'n_estimators': [20],
-#     'max_depth': [6, 8, 10, 12],
-#     'min_samples_leaf': [8, 12, 18],
-#     'min_samples_split': [8, 16, 20]
-# }
+params = {
+    'n_estimators': [100, 500],
+    'learning_rate': [0.05, 0.1]
+}
 
-# # RandomForestClassifier 객체 생성 후 GridSearchCV 수행 (n_jobs = -1 -> 모든 CPU 코어를 이용해 학습)
-# rf_clf = RandomForestClassifier(random_state=0, n_jobs=-1)
-# grid_cv = GridSearchCV(rf_clf, param_grid=params, cv=2, n_jobs=-1)
-# grid_cv.fit(X_train, y_train)
+grid_cv = GridSearchCV(gb_clf, param_grid=params, cv=2, verbose=1)
+grid_cv.fit(x_train, y_train)
+print('최적 하이퍼 파라미터: ', grid_cv.best_params_)
+print('최고 예측 정확도: {0:.4f}'.format(grid_cv.best_score_))
 
-# print('최적 하이퍼 파라미터: ', grid_cv.best_params_)
-# print('최고 예측 정확도: {0:.4f}'.format(grid_cv.best_score_))
-# 최적 하이퍼 파라미터:  {'max_depth': 12, 'min_samples_leaf': 18, 'min_samples_split': 8, 'n_estimators': 10}
-# 최고 예측 정확도: 0.9032
-
-# 최적 하이퍼 파라미터로 재학습 및 예측/평가
-rf_clf = RandomForestClassifier(n_estimators=10, max_depth=12, min_samples_leaf=18, min_samples_split=8, random_state=0)
-
-rf_clf.fit(X_train, y_train)
-pred = rf_clf.predict(X_test)
-print('예측 정확도: {0:.4f}'.format(accuracy_score(y_test, pred)))
-# 예측 정확도: 0.9006
-
+# GridSearchCV를 이용하여 최적으로 학습된 estimator로 predict 수행
+gb_pred = grid_cv.best_estimator_.predict(x_test)
+gb_accuracy = accuracy_score(y_test, gb_pred)
+print('GBM 정확도: {0:.4f}'.format(gb_accuracy))
